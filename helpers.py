223e1de39eacd046fe76efb64510243485d29c40
Max Heller <heller.max1@gmail.com>
2017-08-15 23:17:52 -0400

    Revised error page

--- helpers.py
+++ helpers.py
@@ -1,22 +1,11 @@
-import csv
 import urllib.request
 from flask import redirect, render_template, request, session, url_for
 from functools import wraps
 
-def apology(top="", bottom=""):
+def apology(message=""):
     """Renders message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
+    return render_template("apology.html", message=message)
 
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-            ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=escape(top), bottom=escape(bottom))
-                    
 # Not currently used
 # def login_required(f):
 #     """
